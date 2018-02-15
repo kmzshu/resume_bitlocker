d1e8f02f513d6f1b23efa6a84158ed09abbdd02e
kmzshu <kmzshu@gmail.com>
2018-02-15 15:30:10 +0100

    Powershell Script resume bitlocker
    Powershell Script resume bitlocker remote computer

---
+++ resume_bitlocker.ps1
@@ -0,0 +1,15 @@
+﻿[CmdletBinding()]
+Param(
+[Parameter(Mandatory=$True)]
+[string]$Computername
+)
+if(Test-Connection -ComputerName $ComputerName -Count 1 -Quiet){
+    #BitLocker enable
+    [wmiclass]$wmi="\\$Computername\root\cimv2:win32_process"
+    $wmi.Create("manage-bde -protectors -enable c:")
+    #BitLocker disable
+    #$wmi.Create("manage-bde -protectors -disable c:")
+}
+else{
+    Write-Host "Nem érhető el a hálózaton $ComputerName!" -ForegroundColor Red
+}
